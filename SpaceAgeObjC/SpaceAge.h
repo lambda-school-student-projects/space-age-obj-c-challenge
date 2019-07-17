75bc5bbc61724a897cf616884dac79444c1b47da
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-07-17 11:48:46 -0400

    Added some code for a space age calcualtion function, but commented it all out due to compiler errors.

--- SpaceAgeObjC/SpaceAge.h
+++ SpaceAgeObjC/SpaceAge.h
@@ -5,5 +5,8 @@
 //  Created by jkaunert on 7/17/19.
 //  Copyright © 2019 joshua kaunert. All rights reserved.
 //
+#import <Foundation/Foundation.h>
 
+@interface SpaceAge : NSObject
 
+@end
--- SpaceAgeObjC/SpaceAge.m
+++ SpaceAgeObjC/SpaceAge.m
@@ -8,3 +8,39 @@
 
 #import <Foundation/Foundation.h>
 
+
+//@implementation SpaceAge
+//
+//
+//
+//
+//- (void) spaceAge:( double )ageInSeconds ( NSString *)planetName {
+//    double earthSeconds = 0;
+//    //Mercury Age
+//    //earthSeconds = ageInSeconds * 0.2408467
+//    if (planetName == "Mercury") {
+//        earthSeconds = ageInSeconds * 0.2408467;
+//        NSLog(@"%f Earth seconds.", earthSeconds);
+//    } else if (planetName == "Venus") {
+//        earthSeconds = ageInSeconds * 0.61519726;
+//        NSLog(@"%f Earth seconds.", earthSeconds);
+//    } else if (planetName == "Mars") {
+//        earthSeconds = ageInSeconds * 1.8808158;
+//        NSLog(@"%f Earth seconds.", earthSeconds);
+//    } else if (planetName == "Jupiter") {
+//        earthSeconds = ageInSeconds * 11.862615;
+//        NSLog(@"%f Earth seconds.", earthSeconds);
+//    } else if (planetName == "Saturn") {
+//        earthSeconds = ageInSeconds * 29.447498;
+//        NSLog(@"%f Earth seconds.", earthSeconds);
+//    } else {
+//        NSLog("Try again");
+//    }
+//
+//}
+//
+//
+//
+//
+//@end
+

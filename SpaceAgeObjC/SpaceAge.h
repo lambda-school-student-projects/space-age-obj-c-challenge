26397f64b7ca2d599e054da7f7c4312ee357b915
John Pitts <jgpitts@gmail.com>
2019-07-17 11:45:01 -0400

    wrote .h file for SpaceAge, then wrote initial needed variable 'secondsInEarthYear' to use with function 'ageOnEarth' function.  All written to pass SpaceAgeObjCTests which was found inside the file

--- SpaceAgeObjC/SpaceAge.h
+++ SpaceAgeObjC/SpaceAge.h
@@ -6,4 +6,34 @@
 //  Copyright © 2019 joshua kaunert. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SpaceAge : NSObject
+
+@property int *age;
+
+@property int *secondsInEarthYear;
+
+@property double *onEarth;
+@property double *onMercury;
+@property double *onVenus;
+@property double *onMars;
+@property double *onJupiter;
+@property double *onSaturn;
+@property double *onUranus;
+@property double *onNeptune;
+
+- (double)ageOnEarth:(int *)age;
+- (double)ageOnMercury:(int *)age;
+- (double)ageOnVenus:(int *)age;
+- (double)ageOnMars:(int *)age;
+- (double)ageOnJupiter:(int *)age;
+- (double)ageOnSaturn:(int *)age;
+- (double)ageOnUranus:(int *)age;
+- (double)ageOnNeptune:(int *)age;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- SpaceAgeObjC/SpaceAge.m
+++ SpaceAgeObjC/SpaceAge.m
@@ -6,5 +6,21 @@
 //  Copyright © 2019 joshua kaunert. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "Foundation/Foundation.h"
+#import "SpaceAge.h"
+
+@implementation SpaceAge
+
+int secondsInEarthYear 
+
+- (int)ageOnEarth:(int *)age {
+    
+    
+    _onEarth = age /
+    
+    return _onEarth;
+}
+
+
+@end
 

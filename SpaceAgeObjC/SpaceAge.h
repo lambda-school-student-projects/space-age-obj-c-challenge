90bacf340ec630ab2d3e34a67f6d99e889e47d95
Kobe McKee <kobe.mckee@gmail.com>
2019-07-17 09:43:35 -0600

    I miss swift

--- SpaceAgeObjC/SpaceAge.h
+++ SpaceAgeObjC/SpaceAge.h
@@ -1,9 +1,46 @@
 //
 //  SpaceAge.h
-//  SpaceAgeObjC
+//  SpaceAgeObjC_Challenge
 //
-//  Created by jkaunert on 7/17/19.
+//  Created by Kobe McKee on 7/17/19.
 //  Copyright © 2019 joshua kaunert. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SpaceAge : NSObject
+
+
+@property double *ageInYears;
+@property double *ageInSeconds;
+
+@property double *mercuryMultiplier;
+@property double *venusMultiplier;
+@property double *marsMultiplier;
+@property double *jupiterMultiplier;
+@property double *saturnMultiplier;
+@property double *uranusMultiplier;
+@property double *neptuneMultiplier;
+
+@property double *mercuryAgeSeconds;
+@property double *venusAgeSeconds;
+@property double *marsAgeSeconds;
+@property double *jupiterAgeSeconds;
+@property double *saturnAgeSeconds;
+@property double *uranusAgeSeconds;
+@property double *neptuneAgeSeconds;
+
+@property double *mercuryAgeYears;
+@property double *venusAgeYears;
+@property double *marsAgeYears;
+@property double *jupiterAgeYears;
+@property double *saturnAgeYears;
+@property double *uranusAgeYears;
+@property double *neptuneAgeYears;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
--- SpaceAgeObjC/SpaceAge.m
+++ SpaceAgeObjC/SpaceAge.m
@@ -1,10 +1,50 @@
 //
 //  SpaceAge.m
-//  SpaceAgeObjC
+//  SpaceAgeObjC_Challenge
 //
-//  Created by jkaunert on 7/17/19.
+//  Created by Kobe McKee on 7/17/19.
 //  Copyright © 2019 joshua kaunert. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "SpaceAge.h"
 
+@implementation SpaceAge
+
+- (instancetype)initWithAge:(double *)ageInYears {
+    
+    double earthSecondsMultiplier = 31447600.0;
+    double ageInSeconds = ageInYears * earthSecondsMultiplier;
+    
+    double mercuryMultiplier = 0.2308467;
+    double venusMultiplier = 0.61519726;
+    double marsMultiplier = 1.8808158;
+    double jupiterMultiplier = 11.862615;
+    double saturnMultiplier = 29.447498;
+    double uranusMultiplier = 84.016846;
+    double neptuneMultiplier = 164.79132;
+    
+    double mercuryAgeSeconds = ageInSeconds * mercuryMultiplier;
+    double venusAgeSeconds = ageInSeconds * venusMultiplier;
+    double marsAgeSeconds = ageInSeconds * marsMultiplier;
+    double jupiterAgeSeconds = ageInSeconds * jupiterMultiplier;
+    double saturnAgeSeconds = ageInSeconds * saturnMultiplier;
+    double uranusAgeSeconds = ageInSeconds * uranusMultiplier;
+    double neptuneAgeSeconds = ageInSeconds * neptuneMultiplier;
+    
+    double mercuryAgeYears = mercuryAgeSeconds * earthSecondsMultiplier;
+    double venusAgeYears = venusAgeSeconds * earthSecondsMultiplier;
+    double marsAgeYears = marsAgeSeconds * earthSecondsMultiplier;
+    double jupiterAgeYears = jupiterAgeSeconds * earthSecondsMultiplier;
+    double saturnAgeYears = saturnAgeSeconds * earthSecondsMultiplier;
+    double uranusAgeYears = uranusAgeSeconds * earthSecondsMultiplier;
+    double neptuneAgeYears = neptuneAgeSeconds * earthSecondsMultiplier;
+}
+
+
+
+
+
+
+
+
+@end
